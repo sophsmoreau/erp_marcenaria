6ef4d19dcb11f57a1378884f0763206e1a686e7e
gabriel <gabriel.notari@icloud.com>
2025-04-08 16:44:07 -0300

    adição de classes e métodos

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,12 @@
 public class Main {
     public static void main(String[] args) {
-        Cliente cliente = new Cliente("Maria", "111.222.333-44", "(11) 9999-0000");
+        Cliente cliente = new Cliente("Maria", "111.222.333-44", "(11) 9999-0000", "maria.xxx@xxx", "wdwcwc");
         
         boolean cpfValido = ClienteService.validarCPF(cliente.getCpfCnpj());
         System.out.println("CPF válido? " + cpfValido);
-        
-        Pedido pedido = new Pedido(cliente);
+
+        Cliente Cliente = null;
+        Pedido pedido = new Pedido(Cliente);
         pedido.addProduto("Mesa de Madeira");
         System.out.println("Pedido criado para: " + pedido.getCliente().getNome());
     }
--- src/Pedido.java
+++ src/Pedido.java
@@ -2,16 +2,49 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Pedido {
+    private int codigoPedido;
+    private int data;
+    private double valor;
+    private String status;
     private Cliente cliente;
     private List<String> produtos = new ArrayList<>();
 
-    public Pedido(Cliente cliente) {
+    public Pedido( Cliente cliente) {
         this.cliente = cliente;
+    }
+
+    public Pedido(int codigoPedido, int data, double valor, String status) {
+        this.codigoPedido = codigoPedido;
+        this.data = data;
+        this.valor = valor;
+        this.status = status;
     }
 
     public void addProduto(String produto) {
         produtos.add(produto);
     }
 
-    public Cliente getCliente() { return cliente; }
+    public Cliente getCliente(){
+        return cliente;
+    }
+
+    public int getCodigoPedido() {
+        return codigoPedido;
+    }
+
+    public int getData() {
+        return data;
+    }
+
+    public double getValor() {
+        return valor;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public double calcularTotal(){
+        return 0;
+    }
 }
