0e42e5924fe5f6a030d5b1fde313aef1396d58c9
gabriel <gabriel.notari@icloud.com>
2025-05-12 15:52:19 -0300

    Implementação classe EstoqueService e PedidoService

--- src/service/EstoqueService.java
+++ src/service/EstoqueService.java
@@ -1,4 +1,12 @@
 package service;
 
+import repository.ProdutoRepository;
+
 public class EstoqueService {
+    private final ProdutoRepository produtoRepo = ProdutoRepository.getInstance();
+
+    public void exibirEstoque() {
+        System.out.println("--- ESTOQUE ---");
+        produtoRepo.listarProdutos().forEach(System.out::println);
+    }
 }
--- src/service/PedidoService.java
+++ src/service/PedidoService.java
@@ -1,11 +1,56 @@
 package service;
 
+import model.Cliente;
+import model.Pedido;
+import model.Produto;
+import repository.ClienteRepository;
+import repository.PedidoRepository;
+import repository.ProdutoRepository;
+
 import java.util.Scanner;
 
 public class PedidoService {
+    private final PedidoRepository pedidoRepo = PedidoRepository.getInstance();
+    private final ProdutoRepository produtoRepo = ProdutoRepository.getInstance();
+    private final ClienteRepository clienteRepo = ClienteRepository.getInstance();
+
     public void realizarPedido(Scanner scanner) {
+        System.out.print("ID do cliente: ");
+        int idCliente = scanner.nextInt();
+        Cliente cliente = clienteRepo.buscarPorId(idCliente);
+        if (cliente == null) {
+            System.out.println("Cliente não encontrado.");
+            return;
+        }
+
+        System.out.println("Produtos disponíveis:");
+        produtoRepo.listarProdutos().forEach(System.out::println);
+
+        System.out.print("ID do produto: ");
+        int idProduto = scanner.nextInt();
+        Produto produto = produtoRepo.buscarPorId(idProduto);
+
+        if (produto == null) {
+            System.out.println("Produto não encontrado.");
+            return;
+        }
+
+        System.out.print("Quantidade desejada: ");
+        int quantidade = scanner.nextInt();
+
+        if (produto.getQuantidade() < quantidade) {
+            System.out.println("Estoque insuficiente.");
+            return;
+        }
+
+        produto.setQuantidade(produto.getQuantidade() - quantidade);
+        Pedido pedido = new Pedido(cliente, produto, quantidade);
+        pedidoRepo.adicionarPedido(pedido);
+
+        System.out.println("Pedido realizado com sucesso.");
     }
 
     public void consultarPedidos() {
+        pedidoRepo.listarPedidos().forEach(System.out::println);
     }
 }
